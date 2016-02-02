0db5defd956fdb27a113f91bebd42fb219940d70
philiptang42 <philiptang42@gmail.com>
2016-02-01 21:10:07 -0500

    second if statement

--- dragon_slayer.js
+++ dragon_slayer.js
@@ -1,13 +1,20 @@
 slaying = true;
 youHit = Math.floor(Math.random() * 2);
-damageThisround = Math.floor(Math.random() * 5 + 1);
+damageThisRound = Math.floor(Math.random() * 5 + 1);
 totalDamage = 0;
 
 while(slaying) {
-  if (youHit) {
+  if(youHit) {
     console.log("You hit the dragon!");
+      totalDamage += damageThisRound;
+      if(totalDamage >= 4) {
+        console.log("You have slain the dragon!");
+        slaying = false;
+      } else {
+        youHit = Math.floor(Math.random() * 2);
+      }
   } else {
     console.log("You missed! Big mistake.");
   }
-  slaying = false;
+  slaying = false
 }
